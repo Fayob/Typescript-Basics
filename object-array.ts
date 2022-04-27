77a2048cd688e4acb412093c1439afbae963c488
= <adedejifavour6@gmail.com>
2022-04-27 13:26:55 +0400

    Objects and Array in Typesripts

---
+++ object-array.ts
@@ -0,0 +1,24 @@
+// const person: {
+//     name: string;
+//     age: number
+// } = {
+    const person =  {
+       name: "Fayob",
+        age: 30,
+        hobbies: ["Sports", "Learning"],
+    }
+    
+    let favoriteActivity: string[]
+    
+    favoriteActivity = ["Sports"]
+    favoriteActivity.push("Writing")
+    
+    console.log(favoriteActivity);
+    console.log(favoriteActivity[3]);
+    console.log(person.name);
+    
+    for (const hobby of person.hobbies) {
+        console.log(hobby);
+        
+    }
+    
