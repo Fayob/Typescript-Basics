6328ea4c71edfd61a3e30dccc0ed9a884204de55
= <adedejifavour6@gmail.com>
2022-04-27 13:22:05 +0400

    Function Types and void

---
+++ function-void.ts
@@ -0,0 +1,10 @@
+function add(n1: number, n2: number) {
+    return n1 + n2
+}
+
+function printResult(num: number){
+    console.log("Result:" + num);
+}
+
+console.log(printResult(add(5, 12)));
+ 
